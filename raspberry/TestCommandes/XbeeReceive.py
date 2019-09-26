7fbfa971f364ebb03ceb0588c32a71e33221efa6
Team Drogon <meis@etud.insa-toulouse.fr>
2019-09-26 03:50:33 +0200

    Added XBee send and receive scripts

---
+++ raspberry/TestCommandes/XbeeReceive.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import time
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyUSB0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1             
+ )
+counter=0       
+      
+while 1:
+    #ser.write(str.encode('Write counter: %d \n'%(counter)))
+    #time.sleep(1)
+    #counter += 1
+    x=ser.readline().strip()    
+    
+    x = x.decode("utf-8")
+    
+    print(x)
+    

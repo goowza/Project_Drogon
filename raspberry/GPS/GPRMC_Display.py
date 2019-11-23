0895c547808c88c0e46526198ec63fb2a590ec85
TeamMoscou <TeamMoscou@gmail.com>
2019-11-23 11:13:23 +0000

    Added feature to parse lat and long from GPRMC message

--- raspberry/GPS/GPRMC_Display.py
+++ raspberry/GPS/GPRMC_Display.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 
 # Creates serial link with XBee module
 try:
-    ser = serial.Serial(
+	ser = serial.Serial(
         port=args.serial_port,
         baudrate=9600,
         parity=serial.PARITY_NONE,
@@ -25,15 +25,25 @@ try:
         timeout=1
     )
 except:
-    print("Error creating the serial link")
+	print("Error creating the serial link")
 
 while 1:
 
-    # Read message received on the XBee
-    raw_data=ser.readline().strip()
+	# Read message received from GPS
+	raw_data=ser.readline().strip()
     
-    raw_data = raw_data.decode()
-    
-    if "$GPRMC" in raw_data:
-        print(raw_data)
-
+	raw_data = raw_data.decode()
+	if raw_data != "":
+		# GPRMC is the part of the NMEA sentence we need
+		if "$GPRMC" in raw_data:
+			# Format : $GPRMC,22056,A,5133.82,N,00042.24,W,173.8 ...
+			raw_data = raw_data.split(",")
+			if raw_data[2] == "A":
+				latitude = float(raw_data[3]) / 10.0
+				longitude = float(raw_data[5]) / 10.0
+			else:
+				latitude = -1
+				longitude = -1
+			print("Lat : {} | Long : {}".format(latitude,longitude), end="\r")
+	else:
+		print("No GPS data available")

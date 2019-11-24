c181e895f190feb38f5b88c8c5316b2122f38124
Titguig <titguig@gmail.com>
2019-11-24 21:52:43 +0100

    Modification du client dans le but de faire l'integration. Rajout d'un thread de synchro pour declencher l'envoi de message au serveur sur evenement

--- raspberry/server/clienttest.py
+++ raspberry/server/clienttest.py
@@ -2,10 +2,39 @@
  # Definition d'un client reseau gerant en parallele l'emission
  # et la reception des messages (utilisation de 2 THREADS).
 
-host = '10.1.5.190'
-port = 40001
+#host = '10.1.5.190'
+#port = 40001
 
+host='127.0.0.1'
+port=9999
 import socket, sys, threading
+import time
+
+
+class ThreadSync(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.lock = lock
+
+    def run(self):
+         global message_emis
+         a=0
+         self.lock.acquire()
+         while 1:
+             if input()=="S":
+                 a=a+1
+                 message_emis=message_emis+str(a)
+                 print(message_emis)
+                 self.lock.release()
+                 print("Relache")
+                 self.lock.acquire()
+                 print("Ack")
+
+
+
+
+
 
 class ThreadReception(threading.Thread):
    """objet thread gerant la reception des messages"""
@@ -29,12 +58,16 @@ class ThreadEmission(threading.Thread):
     """objet thread gerant l'emission des messages"""
     def __init__(self, conn):
         threading.Thread.__init__(self)
+        self.lock = lock
         self.connexion = conn           # ref. du socket de connexion
         
     def run(self):
+        global message_emis
         while 1:
-            message_emis = input()
+            self.lock.acquire()
             self.connexion.send(message_emis.encode('utf-8'))
+            self.lock.release()
+            time.sleep(0.1)
 
 # Programme principal - etablissement de la connexion :
 connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,7 +80,11 @@ print("Connexion etablie avec le serveur.")
             
 # Dialogue avec le serveur : on lance deux threads pour gerer
 # independamment l'emission et la reception des messages :
+message_emis ="A5B69" #Variable globale
+lock = threading.Lock()
+th_S= ThreadSync()
 th_E = ThreadEmission(connexion)
 th_R = ThreadReception(connexion)
+th_S.start()
 th_E.start()
 th_R.start()        

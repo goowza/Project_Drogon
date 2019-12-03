6baabab5fe0c8ffbf20f9f28d7469c52873a8242
Team Drogon <meis@etud.insa-toulouse.fr>
2019-12-03 14:38:47 +0100

    Amelioration du receive

--- raspberry/XBee/XbeeReceiveAPI.py
+++ raspberry/XBee/XbeeReceiveAPI.py
@@ -6,11 +6,24 @@ import os
 import time
 import argparse
 from xbee import XBee
+import RPi.GPIO as GPIO
 
 # Manage arguments used when launching the script
 parser = argparse.ArgumentParser()
 parser.add_argument("serial_port", help="serial port of XBee module")
+parser.add_argument("--num_pin", type=int)
 args = parser.parse_args()
+
+# Parametre les GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+if args.num_pin:
+    LED = args.num_pin
+else:
+    LED = 7
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, GPIO.LOW)
 
 # Compte le nombre de messages recus
 count = 0
@@ -26,8 +39,17 @@ def print_data(data):
     """
     global count
     count = count + 1
+
     try:
-        print("RSSI value : {} ({})".format(ord(data["rssi"].decode()), count), end="\r")
+        print("RSSI value : {} Data value : {} ({})".format(ord(data["rssi"].decode()), data["rf_data"], count), end="\r")
+        ID = data["rf_data"].decode().split(":")[0]
+        if ID == "HL118":
+            RSSI = ord(data["rssi"].decode())
+            if RSSI < 60:
+                GPIO.output(LED, GPIO.HIGH)
+            else:
+                GPIO.output(LED, GPIO.LOW)
+        
     except:
         print("GNEUGNEU")
 

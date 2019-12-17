6af9586a6694499cbb1896d78f417551d3e34776
drogon <projetdrogon@gmail.com>
2019-12-17 14:14:53 +0100

    Added script for the bollards

--- raspberry/Demo_Sprint4/ScriptBorne.py
+++ raspberry/Demo_Sprint4/ScriptBorne.py
@@ -7,12 +7,20 @@ import time
 import argparse
 from xbee import XBee
 import RPi.GPIO as GPIO
+import pygame, picamera
+import traceback
 
 # Manage arguments used when launching the script
 parser = argparse.ArgumentParser()
 parser.add_argument("serial_port", help="serial port of XBee module")
 args = parser.parse_args()
 
+pygame.init()
+res = pygame.display.list_modes() # return the best resolution for your monitor
+width, height = res[0] # Having trouble getting the right resolution? Manually set with: 'width, height = 1650, 1050' (where the numbers match your monitor)
+camera = picamera.PiCamera()
+camera.resolution = (width, height)
+      
 # Parametre les GPIO
 
 GPIO.setmode(GPIO.BOARD)
@@ -25,6 +33,16 @@ GPIO.output(Gyro, GPIO.LOW)
 # Compte le nombre de messages recus
 count = 0
 
+def take_pic(pics_path):
+    """Saves picture by overwriting previously saved pic"""
+    try:
+        if os.path.isfile(pics_path):
+            os.remove(pics_path)
+        camera.capture(pics_path, use_video_port = True)
+        #sendtoserver
+    except:
+        print("erreur capture")
+            
 def print_data(data):
     """
     This method is called whenever data is received
@@ -34,13 +52,13 @@ def print_data(data):
     data = dictionnaire avec les champs :
     {'id': 'rx', 'source_addr': b'\x00\x04', 'rssi': b'>', 'options': b'\x02', 'rf_data': b'Hey'}
     """
+    pics_path = os.path.join('pics', 'record.jpg')
     global count
     count = count + 1
 
     try:
-        #print(data)
-        #print("RSSI value : {} Data value : {} ({})".format(ord(data["rssi"].decode()), data["rf_data"], count), end="\r")
         ID = data["rf_data"].decode().split(":")[0]
+        command = data["rf_data"].decode().split(":")[1]
         if ID == "HL118":
             RSSI = ord(data["rssi"].decode())
             if RSSI < 60:
@@ -49,23 +67,28 @@ def print_data(data):
             else:
                 print("loin")
                 GPIO.output(Gyro, GPIO.LOW)
-                
+        elif command == "1":
+            take_pic(pics_path)
+            
     except:
         print("GNEUGNEU")
 
-try:
-    serial_port = serial.Serial(args.serial_port, 9600)
-    xbee = XBee(serial_port, callback=print_data)
-except:
-    print("Error creating serial link")
-    exit()
-
-while True:
+if __name__=="__main__":
     try:
-        time.sleep(0.001)
-    except KeyboardInterrupt:
-        break
+        serial_port = serial.Serial(args.serial_port, 9600)
+        xbee = XBee(serial_port, callback=print_data)
+    except:
+        print("Error creating serial link")
+        exit()
 
-print("\n")
-xbee.halt()
-serial_port.close()
+    while True:
+        try:
+            time.sleep(0.001)
+        except KeyboardInterrupt:
+            break
+
+    print("\n")
+    camera.close()
+    pygame.quit()
+    xbee.halt()
+    serial_port.close()
